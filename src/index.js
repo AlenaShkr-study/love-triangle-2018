3a541a4294f311a7914591ba5b5653965575d247
AlenaShkr <elalshkr@gmail.com>
2018-09-16 20:38:16 +0300

    love-triangle: add method finding count of lovetriangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,30 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+
+  for ( let i=1; i<= preferences.length-2; i++)
+  {
+    
+    
+    if  ( (preferences[i-1]>preferences.length) ||
+     (preferences[i-1]==i))
+     {
+      continue;
+      }
+
+    if    ( (preferences[preferences[preferences[i-1]-1]-1]==i) 
+          && (preferences[i-1]>i) && preferences[preferences[i-1]-1]>i)
+/* secondValue=preferences[i-1];
+ thirdValue=preferences[secondValue-1]=preferences[preferences[i-1]-1];
+firstValue=preferences[thirdValue-1]= preferences[preferences[preferences[i-1]-1]-1]=
+if ((firstValue== i) && (secondValue>i) && (thirdValue>i)) {count++}*/
+       {
+      count++;
+      };
+    
+     };
+   
+     return count;
+
 };
